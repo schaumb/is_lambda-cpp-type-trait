111219eff31dbcacddc1a4eb28ec7f07ad4e8617
Bela Schaum <bela.schaum@sunstone-rtls.com>
2020-01-14 12:59:30 +0100

    add static_asserts + fix clang funptr with return lambda

---
+++ assert_tests.cpp
@@ -0,0 +1,71 @@
+#include "is_lambda.hpp"
+
+#include <iostream>
+#include <functional>
+#include <cmath>
+
+struct NotLambda {
+    void operator()() {}
+};
+
+constexpr auto l = [](){};
+
+template<typename T, typename ...Us>
+auto templateLambda = [](auto&&, Us&&...) -> T { return T{}; };
+
+constexpr auto funWithReturnLambda() {
+    return [](auto&&) { return 42; };
+}
+
+auto lamdaInsideLamda = [a = []{}] { return a; };
+
+template<typename T>
+auto operator<(T, int) {
+    return [] (T&) { return 42; };
+}
+
+template<typename T>
+auto x(T) { return []{}; }
+
+template<typename T>
+struct A {
+    auto operator>(int a) {
+        return []{} < a;
+    }
+};
+
+int main() {
+    auto localLambda = [] (int, char) -> unsigned { return 0; };
+    
+    static_assert(IS_LAMBDA([](){}), "lambda");
+    static_assert(IS_LAMBDA(l), "lambda");
+    static_assert(IS_LAMBDA(templateLambda<int>), "lambda");
+    static_assert(IS_LAMBDA(funWithReturnLambda()), "lambda");
+    static_assert(IS_LAMBDA(lamdaInsideLamda()), "lambda");
+    static_assert(IS_LAMBDA(A<double>{} > 5), "lambda");
+    static_assert(IS_LAMBDA(localLambda), "lambda");
+
+    static_assert(!IS_LAMBDA(5 - 6), "not lambda");
+    static_assert(!IS_LAMBDA([]{}()), "not lambda");
+    static_assert(!IS_LAMBDA(NotLambda{}), "not lambda");
+    static_assert(!IS_LAMBDA(&NotLambda::operator()), "not lambda");
+    static_assert(!IS_LAMBDA(A<decltype(l)>{}), "not lambda");
+    static_assert(!IS_LAMBDA(&::template x <int>), "not lambda");
+    static_assert(!IS_LAMBDA(&::template operator< <decltype(l)>), "not lambda");
+    static_assert(!IS_LAMBDA(&std::ceilf), "not lambda");
+
+    static_assert(bxlx::is_lambda_v<decltype(l)>, "lambda");
+    static_assert(bxlx::is_lambda_v<decltype(templateLambda<int>)>, "lambda");
+    static_assert(bxlx::is_lambda_v<std::invoke_result_t<decltype(&funWithReturnLambda)>>, "lambda");
+    static_assert(bxlx::is_lambda_v<std::invoke_result_t<decltype(lamdaInsideLamda)>>, "lambda");
+    static_assert(bxlx::is_lambda_v<decltype(localLambda)>, "lambda");
+    
+    static_assert(!bxlx::is_lambda_v<NotLambda>, "not lambda");
+    static_assert(!bxlx::is_lambda_v<decltype(NotLambda{})>, "not lambda");
+    static_assert(!bxlx::is_lambda_v<decltype(&NotLambda::operator())>, "not lambda");
+    static_assert(!bxlx::is_lambda_v<decltype(std::bind(NotLambda{}))>, "not lambda");
+    static_assert(!bxlx::is_lambda_v<std::remove_pointer_t<decltype(&::template x <int>)>>, "not lambda");
+    static_assert(!bxlx::is_lambda_v<std::remove_pointer_t<decltype(&::template operator< <decltype(l)>)>>, "not lambda");
+    static_assert(!bxlx::is_lambda_v<std::remove_pointer_t<decltype(&std::ceilf)>>, "not lambda");
+    return 0;
+}
--- is_lambda.hpp
+++ is_lambda.hpp
@@ -9,30 +9,35 @@ namespace bxlx {
         constexpr bool is_lambda() {
             constexpr std::string_view wholeName = nameof::nameof_type<T>();
 #if defined(__clang__)
-            return wholeName.rfind("(lambda at ", 0) == 0 && *wholeName.rbegin() == ')';
+            // format: "(lambda at filename:line:char)"
+            return wholeName.rfind("(lambda at ", 0) == 0 && *wholeName.rbegin() == ')' 
+                && wholeName.find(':') > wholeName.rfind('('); // This condition filters function pointers/references which returns lambda. 
+                // '(' can be part of filename, but ':' is not. 
 #elif defined(__GNUC__)
             static_assert(false, "TODO it's harder than I thought first");
 #elif defined(_MSC_VER)
+            // format: "class <lambda_idchars>"
             return wholeName.rfind("class <lambda_", 0) == 0 && *wholeName.rbegin() == '>';
 #endif
         }
     }
 
     template<typename T>
-    struct is_lambda : std::integral_constant<bool, impl::is_lambda<std::decay_t<T>>()> {};
+    struct is_lambda : std::integral_constant<bool, impl::is_lambda<T>()> {};
 
     template<typename T>
-    constexpr bool is_lambda_v = typename is_lambda<T>::value;
+    constexpr bool is_lambda_v = is_lambda<T>::value;
 }
 
 #ifdef IS_LAMBDA
 #undef IS_LAMBDA
 #endif // IS_LAMBDA
 
-#define IS_LAMBDA(x)                                        \
-[]{                                                         \
-    auto&& v = x;                                           \
-    return bxlx::is_lambda_v<std::decay_t<decltype(v)>>;    \
+#define IS_LAMBDA(x)                                                    \
+[&]{                                                                    \
+    [[maybe_unused]]                                                    \
+    auto&& _lname = [&]() -> decltype(auto) { return x; };              \
+    return bxlx::is_lambda_v<std::invoke_result_t<decltype(_lname)>>;   \
 }()
 
 #endif // BXLX_IS_LAMBDA_INCLUDED_HPP
